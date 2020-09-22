31b40e394da1fc24c785f6d2e66cbdcf6e945701
Martin <maiste.martin@gmail.com>
2020-09-22 10:55:59 +0300

    Ülesanne 2.2

---
+++ 2/yl2.py
@@ -0,0 +1,9 @@
+perekonnanimi = input("Sisestage leedu perekonnanimi: ")
+if perekonnanimi[-2:] == "ne":
+    print("Abielus")
+elif perekonnanimi[-2:] == "te":
+    print("Vallaline")
+elif perekonnanimi[-1:] == "e":
+    print("Määramata")
+else:
+    print("Pole ilmselt leedulanna perekonnanimi")
